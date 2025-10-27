982a263defa8c582028a080fe03c8c321aeca017
NakajimaTomoya36269 <oibc36269@ojs.ac.jp>
2025-10-27 12:10:31 +0900

    タイトルシーン途中

---
+++ 36269_中嶋智也_個人作成用/game/src/game_manager/game_object/scene_manager/scene/title/title.cpp
@@ -0,0 +1,21 @@
+#include "title.h"
+
+CTitle::CTitle(void)
+{
+}
+
+void CTitle::Initialize(void)
+{
+}
+
+void CTitle::Update(void)
+{
+}
+
+void CTitle::Draw(void)
+{
+}
+
+void CTitle::Finalize(void)
+{
+}
--- 36269_中嶋智也_個人作成用/game/src/game_manager/game_object/scene_manager/scene/title/title.h
+++ 36269_中嶋智也_個人作成用/game/src/game_manager/game_object/scene_manager/scene/title/title.h
@@ -1,1 +1,26 @@
 #pragma once
+
+#include "../scene.h"
+
+class CTitle
+	: public IScene
+{
+public:
+	// コンストラクタ
+	CTitle(void);
+
+	// デストラクタ
+	~CTitle(void) = default;
+
+	// 初期化
+	void Initialize(void) override;
+
+	// 更新
+	void Update(void) override;
+
+	// 描画
+	void Draw(void) override;
+
+	// 解放
+	void Finalize(void) override;
+};
